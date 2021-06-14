7e49d6723c301bb7b6e67be1d0486f1621928f87
shreya-1771 <84712068+shreya-1771@users.noreply.github.com>
2021-06-14 20:04:38 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -32,3 +32,54 @@ float average(int x[], int n){
   avg= (float)sum/n;
   return avg;
 }
+
+int mode(int x[], int n){
+ int arr[100];
+ for (int i=0; i<100; i++{
+  arr[i]=0;
+ }
+      for (int i=0;i<n;i++){
+       arr[x[i]]++;
+      }
+      
+      for (int i=0;i<100;i++){
+       if arr[i]>a[i+1]{
+       lar=i;
+      }
+       return lar;
+      }
+ 
+ int factors(int a, int x[]){
+  int j=0;
+  for (int i=1;i<a;i++){
+   
+   if (a%i==0){
+    x[j]=i
+     j++;
+   }
+  }
+  int n= sizeof(x)/sizeof(x[0]);
+  for(int i=0; i<n; i++){
+   int count++;
+  }
+  
+  return count;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
