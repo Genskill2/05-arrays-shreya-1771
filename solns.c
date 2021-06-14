6a1a0f844082d2f93553c00b8dac9d693210d44a
shreya-1771 <84712068+shreya-1771@users.noreply.github.com>
2021-06-14 19:25:31 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int x[], int sol){
+  int sol=0;
+  int n= sizeof(x)/sizeof(x[0]);
+  for (int i=0;i<n;i++){
+    if(x[i]<x[i+1]){
+      sol= x[i+1];
+    }
+  }
+  return sol;
+}
